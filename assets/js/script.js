d4e46977d084a98a73473b557366390af07181fd
Biz <github@bisrat.dev>
2023-03-30 21:30:53 -0600

    add foundations

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ const APIKEY = 'd6d727c8c68c5cd31db30ad289ac8254';
 const LIMIT = '5';
 // Wrap code in a call to jQuery so all elments on the page will load before we do anything
 $(function(){
+    let weatherSearchHistory = []
+    // if search history is stored locally
+    if(localStorage.getItem('weatherSearchHistory')){
+        for(let searchItem of localStorage.getItem('weatherSearchHistory')){
+            weatherSearchHistory.push(searchItem);
+        }
+    }
     $('#search-btn').click(function (e) { 
         e.preventDefault();
         var userSearchedCity = $('#citySearch').val();
@@ -22,10 +29,13 @@ $(function(){
                 let lon = String(Number(response[1]['lon']).toFixed(2));
                 console.log('lat:'+lat + ' lon:' + lon);
                 // get 5 day forecast from Open Weather Map using lat, long, and the apikey
-                $.ajax({
+                $.ajax({ 
                     type: 'GET',
                     url: `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`
                 }).then(function(response){
+                    weatherSearchHistory.push(userSearchedCity);
+                    $('#placeholder-text-h2').css('display', 'none');
+                    console.log(response['list'][4])
                     console.log(response);
                 });
             });
